92cda17ea8432d397101947fa2d247e31d85bc4a
Safar Galimzianov <safargalimzianov@protonmail.com>
2025-01-08 21:41:18 +0500

    16.1.2: fix dict mistake

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -115,7 +115,7 @@ def add_url():
             flash('Некорректный URL', 'danger')
             return render_template('main/search.html'), 422
 
-        url_id = repo.get_url_by_name(url)
+        url_id = repo.get_url_by_name(url)['id']
         if url_id:
             flash('Страница уже существует', 'info')
             return redirect(url_for('get_url', url_id=url_id))
