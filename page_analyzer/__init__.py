66971eb736554a62f9cd7f31614a5ab6774c4f0a
Safar Galimzianov <safargalimzianov@protonmail.com>
2025-01-11 23:12:20 +0500

    17.2: fix typo in __init__.py

--- page_analyzer/__init__.py
+++ page_analyzer/__init__.py
@@ -1,6 +1,7 @@
 from .app import app
 from .service import make_request, sanitize_url_input
 from .app_repository import AppRepository
+from .db_pool import ConnectionPool
 
 __all__ = [
         'app',
